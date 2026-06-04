3cb92b8460a996b04e87c5d4693019b8ea8d7605
vtuberfestchile <vtuberfestchile@gmail.com>
2026-06-04 00:08:59 -0400

    3th Update
    3th Update

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "VRothers E.I.R.L.-VRothers Festa-0.1";
+const contentToCache = [
+    "Build/5336a4b2c43054286fd70b1faa467eee.loader.js",
+    "Build/015366220fc531d0a69f2f775b2daa5c.framework.js.unityweb",
+    "Build/e053996b17b62de3a09437576e101bfc.data.unityweb",
+    "Build/d238fbd1dca7fc84f3bb69969c19a78d.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
