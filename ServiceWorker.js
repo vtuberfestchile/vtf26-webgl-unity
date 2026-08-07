5ad331a1ae2cfab5cc5f40a35c948c9228daf109
vtuberfestchile <vtuberfestchile@gmail.com>
2026-08-07 11:25:13 -0400

    Mover archivos .data a Git LFS

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "VRothers E.I.R.L.-VRothers Festa-0.1";
 const contentToCache = [
-    "Build/5336a4b2c43054286fd70b1faa467eee.loader.js",
-    "Build/9365b0e2a77859a3fcb799d91ebc7131.framework.js.unityweb",
-    "Build/fdc9dab8d7c40dc5aadf23f67b583971.data.unityweb",
-    "Build/ab91215730133214f3778a39f840fba6.wasm.unityweb",
+    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
+    "Build/9365b0e2a77859a3fcb799d91ebc7131.framework.js",
+    "Build/324eaff09447383b8916e754a3fba50d.data",
+    "Build/ab91215730133214f3778a39f840fba6.wasm",
     "TemplateData/style.css"
 
 ];
