1bfe1c7de95e71fa621972f40c9982e5bb63a9a9
vtuberfestchile <vtuberfestchile@gmail.com>
2026-08-07 11:46:22 -0400

    Ultimas subidas con menos peso
    Less Size

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "VRothers E.I.R.L.-VRothers Festa-0.1";
 const contentToCache = [
-    "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
-    "Build/9365b0e2a77859a3fcb799d91ebc7131.framework.js",
-    "Build/324eaff09447383b8916e754a3fba50d.data",
-    "Build/ab91215730133214f3778a39f840fba6.wasm",
+    "Build/9853637125e801e9aae48e78dbbdcfca.loader.js",
+    "Build/9365b0e2a77859a3fcb799d91ebc7131.framework.js.unityweb",
+    "Build/5a2bb7d597872cfa05d26664e73e8662.data.unityweb",
+    "Build/ab91215730133214f3778a39f840fba6.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
